04808354fe3f275bb88ae502b448e374da4ed5bc
victor leung <thevictor2225@gmail.com>
2025-06-20 23:54:13 -0700

    initial commit

---
+++ src/MyPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MyPage() {
+  return (
+    <div className="page-container">
+      <header>
+        <h1>My React Page</h1>
+      </header>
+      <main>
+        <p>Welcome to your new React page!</p>
+      </main>
+    </div>
+  );
+}
+
+export default MyPage;
